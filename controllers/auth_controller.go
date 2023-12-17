6d383ce6b501019ea68025f92a3e58b2d89ae329
syunsuke-I <odol@ymail.ne.jp>
2023-12-18 07:52:48 +0900

    htmlを返すところまで

---
+++ controllers/auth_controller.go
@@ -0,0 +1,16 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Login(c *gin.Context) {
+
+	c.HTML(
+		http.StatusOK,
+		"index.html",
+		gin.H{},
+	)
+}
--- main.go
+++ main.go
@@ -1,27 +1,14 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
-)
 
-type Status struct {
-	Status string `json:"status"`
-}
+	"github.com/syunsuke-I/golang_twitter/controllers"
+)
 
 func main() {
 	router := gin.Default()
-
-	status := Status{
-		Status: "ok",
-	}
-
-	router.GET("/health_check", func(ctx *gin.Context) {
-		ctx.JSON(200, status)
-	})
-
-	if err := router.Run(":8080"); err != nil {
-		log.Fatal("Server Run Failed.: ", err)
-	}
+	router.LoadHTMLGlob("frontend/templates/*.html")
+	router.GET("/login", controllers.Login)
+	router.Run("0.0.0.0:8080")
 }
